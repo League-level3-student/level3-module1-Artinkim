9ea500bd14a45fac213e06c1bf91a01e334183cc
Artin <Artin@DESKTOP-L54PHGT.san.rr.com>
2018-12-25 12:37:20 -0800

    IPod

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,31 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
+	ArrayList<Song> LS = new ArrayList<Song>();
+	Random rand = new Random();
+	int s;
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+		JFrame frame = new JFrame();
+		JButton b = new JButton("Shuffle");
+
+		frame.add(b);
+		frame.setVisible(true);
+		b.addActionListener(this);
+		LS.add(new Song("demo.mp3"));
+		LS.add(new Song("demo.mp3"));
+		frame.pack();
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -23,5 +38,17 @@ public class _06_IPodShuffle{
 	
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		for(Song s:LS) {
+			s.stop();
+		}
+		s = rand.nextInt(LS.size());
+		LS.get(s).setDuration(10);
+		LS.get(s).play();
+			
 	}
 }
