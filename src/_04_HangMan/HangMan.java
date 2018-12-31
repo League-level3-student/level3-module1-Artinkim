e5b625fbbf3f8acb2c7e0c425a963ddfb852d014
GT <league@wts14-4.attlocal.net>
2018-12-30 17:31:05 -0800

    Working on String integer pairs

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,7 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -9,58 +10,122 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan implements KeyListener{
+public class HangMan implements KeyListener {
 	public static void main(String[] args) {
 		new HangMan().Start();
 	}
+
+	int numW;
+	String S;
+	String numS;
 	JFrame frame = new JFrame();
-	JPanel panel = new JPanel(); 
+	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JLabel label2 = new JLabel();
 	Utilities U = new Utilities();
-	String p;
+	String SCurrent;
+	int lives = 10;
+	int response = 2;
+	ArrayList<Character> Current_Word = new ArrayList<Character>();
 	Stack<String> SS = new Stack<String>();
 	Stack<Character> Word = new Stack<Character>();
+	ArrayList<Integer> MoreOne = new ArrayList<Integer>();
+
+	HangMan() {
+		frame.add(panel);
+		panel.add(label);
+		panel.add(label2);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+	}
+
 	void Start() {
-		String numS= JOptionPane.showInputDialog("How many words do you want to guess");
-		int numW = Integer.parseInt(numS);
-		String S;
-		for(int i = 0;i<numW;i++) {
+		numS = JOptionPane.showInputDialog("How many words do you want to guess");
+		numW = Integer.parseInt(numS);
+		lives = 10;
+		label2.setText(lives + "");
+		for (int i = 0; i < numW; i++) {
 			S = U.readRandomLineFromFile("dictionary.txt");
 			while (SS.contains(S) == true) {
-			S = U.readRandomLineFromFile("dictionary.txt");
+				S = U.readRandomLineFromFile("dictionary.txt");
 			}
 			SS.push(S);
 		}
-		frame.add(panel);
-		panel.add(label);
-		frame.addKeyListener(this);
-		frame.setVisible(true);
-		p = SS.pop();
-		for(int i = 0;i<p.length();i++) {
-			label.setText(label.getText()+" _");
+		SCurrent = SS.pop();
+		for (int i = 0; i < SCurrent.length(); i++) {
+			Current_Word.add('_');
+			Current_Word.add(' ');
+		}
+		for (int i = 0; i < Current_Word.size(); i++) {
+			label.setText(label.getText() + Current_Word.get(i));
 		}
 		frame.pack();
-		
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		if(p.contains(arg0.getKeyChar()+"")) {
-			for(int i = 0;i<p.length();i++) {
-			
+		if (SCurrent.contains(arg0.getKeyChar() + "")) {
+			for (int i = 0; i < SCurrent.length(); i++) {
+				if (SCurrent.charAt(i) == arg0.getKeyChar()) {
+					MoreOne.add(i * 2);
+				}
+			}
+			for (int i = 0; i < MoreOne.size(); i++) {
+				Current_Word.set(MoreOne.get(i), arg0.getKeyChar());
+			}
+			MoreOne.clear();
+			label.setText("");
+			for (int i = 0; i < Current_Word.size(); i++) {
+				label.setText(label.getText() + Current_Word.get(i));
+			}
+		} else {
+			lives--;
+			label2.setText(lives + "");
+			if (lives <= 0) {
+				JOptionPane.showMessageDialog(null, "the word was " + SCurrent);
+				response = JOptionPane.showConfirmDialog(null, "Game Over Would you like to play again");
+				System.out.println(response);
+				if (response == 0) {
+					MoreOne.clear();
+					label.setText("");
+					Current_Word.clear();
+					Start();
+				} else {
+					System.exit(0);
+				}
+
+			}
+		}
+		if (!Current_Word.contains('_')) {
+			MoreOne.clear();
+			label.setText("");
+			Current_Word.clear();
+			SCurrent = SS.pop();
+			lives = 10;
+			label2.setText(lives + "");
+			for (int i = 0; i < SCurrent.length(); i++) {
+				Current_Word.add('_');
+				Current_Word.add(' ');
+			}
+			for (int i = 0; i < Current_Word.size(); i++) {
+				label.setText(label.getText() + Current_Word.get(i));
 			}
 		}
 	}
+
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
